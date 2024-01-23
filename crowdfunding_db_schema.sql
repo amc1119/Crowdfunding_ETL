7de0c1b96e5ab241a8bbc0f0a0851f32402f5303
amc1119 <alexis@Alexiss-MBP.lan>
2024-01-23 01:22:19 -0500

    Update final files to submit.

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -8,24 +8,26 @@
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" string   NOT NULL,
-    "description" string   NULL,
-    "goal" string   NULL,
-    "pledged" int   NOT NULL,
-    "backers_count" string   NOT NULL,
-    "country" string   NOT NULL,
-    "launched_date" datetime   NOT NULL,
-    "end_date" datetime   NOT NULL,
-    "category_id" string   NOT NULL,
-    "subcategory_id" string   NOT NULL,
+    "company_name" varchar(100)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(100)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(100)   NOT NULL,
+    "currency" varchar(100)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(100)   NOT NULL,
+    "subcategory_id" varchar(100)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" string   NOT NULL,
-    "category" string   NOT NULL,
+    "category_id" varchar(100)   NOT NULL,
+    "category" varchar(100)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
@@ -33,17 +35,17 @@ CREATE TABLE "category" (
 
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
-    "first_name" string   NOT NULL,
-    "last_name" string   NOT NULL,
-    "email" string   NOT NULL,
+    "first_name" varchar(100)   NOT NULL,
+    "last_name" varchar(100)   NOT NULL,
+    "email" varchar(100)   NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" string   NOT NULL,
-    "subcategory" string   NOT NULL,
+    "subcategory_id" varchar(100)   NOT NULL,
+    "subcategory" varchar(100)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
      )
@@ -52,12 +54,9 @@ CREATE TABLE "subcategory" (
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
 
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
-
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "campaign" ("category_id");
-
-CREATE INDEX "idx_campaign_contact_id"
-ON "campaign" ("contact_id");
 
